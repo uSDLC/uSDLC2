45d8ee87238d8984ea82e29f97247b01b1be1df1
Paul Marrington <paul@marrington.net>
2012-11-27 18:47:34 +1100

    again

--- release/install-usdlc-on-unix.sh
+++ release/install-usdlc-on-unix.sh
@@ -17,6 +17,7 @@ mkdir -p $ext 2>/dev/null
 # First is uSDLC2 as it includes scripts needed to finish the installation
 ##########################################################################
 curl -sOL https://github.com/uSDLC/uSDLC2/archive/master.zip
+rm -rf uSDLC-master
 unzip -q master.zip
 rm master.zip
 rsync -qrulpt uSDLC2-master/ $base
