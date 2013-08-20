6d6862a3ef67c3ed87884258fe1b0de51e5815a8
Paul Marrington <paul@marrington.net>
2013-08-21 08:37:46 +1000

    install usdlc2

--- go.sh
+++ go.sh
@@ -1,7 +1,7 @@
 #!/bin/bash 
 # Copyright (C) 2013 paul@marrington.net, see uSDLC2/GPL for license
 cd `dirname "${BASH_SOURCE[0]}"`
-if [ ! -f ext/roaster ]; then
+if [ -f ext/roaster ]; then
     local/roaster.sh $@
 else
     ../roaster/go.sh $@
