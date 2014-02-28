e700275caefb751a2431a318606c5dd6c94c3d27
Paul Marrington <paul@marrington.net>
2014-02-28 13:01:49 +1000

    Create easy launch script

--- go.sh
+++ go.sh
@@ -6,3 +6,11 @@ if [ -f ext/roaster ]; then
 else
     ../roaster/go.sh "$@"
 fi
+#!/bin/bash 
+# Copyright (C) 2013 paul@marrington.net, see uSDLC2/GPL for license
+cd `dirname "${BASH_SOURCE[0]}"`
+if [ -f ext/roaster ]; then
+    local/roaster.sh "$@"
+else
+    ../roaster/go.sh "$@"
+fi
--- install-usdlc2.sh
+++ install-usdlc2.sh
@@ -8,3 +8,20 @@ curl -sOL https://raw.github.com/uSDLC/roaster/master/install-roaster.sh
 chmod +x install-roaster.sh
 ./install-roaster.sh
 ./install-roaster.sh . uSDLC2
+cat > uSDLC2.sh << EOF
+#bin/bash
+'$(pwd)/uSDLC2/go.sh' server
+EOF
+chmod +x uSDLC2.sh
+echo $(pwd)/uSDLC2.sh
+$(pwd)/uSDLC2.sh
+#!/bin/bash
+# Copyright (C) 2012,13 paul@marrington.net, see GPL license
+
+parent=${1:-.}
+cd $parent
+echo "Download roaster installation script"
+curl -sOL https://raw.github.com/uSDLC/roaster/master/install-roaster.sh
+chmod +x install-roaster.sh
+./install-roaster.sh
+./install-roaster.sh . uSDLC2
