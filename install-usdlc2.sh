5103c01886a8abec66f834cc6085ba9cb926a156
Paul Marrington <paul@marrington.net>
2014-02-28 12:38:20 +1000

    more legacy references

--- install-usdlc2.sh
+++ install-usdlc2.sh
@@ -14,6 +14,16 @@ chmod +x install-roaster.sh
 parent=${1:-.}
 cd $parent
 echo "Download roaster installation script"
+curl -sOL https://raw.github.com/uSDLC/roaster/master/install-roaster.sh
+chmod +x install-roaster.sh
+./install-roaster.sh
+./install-roaster.sh . uSDLC2
+#!/bin/bash
+# Copyright (C) 2012,13 paul@marrington.net, see GPL license
+
+parent=${1:-.}
+cd $parent
+echo "Download roaster installation script"
 curl -sOL https://raw.github.com/uSDLC/roaster/legacy/install-roaster.sh
 chmod +x install-roaster.sh
 ./install-roaster.sh
